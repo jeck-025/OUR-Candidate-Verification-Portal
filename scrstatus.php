0fdf9a00bb8a62f3d67dc28456fc4aa0f658af79
jeck-025 <jerrickanatalio@gmail.com>
2023-05-16 08:45:44 +0800

    remove excess footer

--- scrstatus.php
+++ scrstatus.php
@@ -65,11 +65,11 @@ $view = new view;
             <div class="small m-0 text-white"><strong>Centro Escolar University</strong>&nbsp&nbsp&nbsp&nbspMariano | Gita | Tuazon | Valencia | Bolasoc | Anatalio</div>
           </div>
           <div class="col-auto">
-            <a class="link-light small" href="#!">Privacy</a>
+            <!-- <a class="link-light small" href="#!">Privacy</a>
             <span class="text-white mx-1">&middot;</span>
             <a class="link-light small" href="#!">Terms</a>
             <span class="text-white mx-1">&middot;</span>
-            <a class="link-light small" href="#!">Contact</a>
+            <a class="link-light small" href="#!">Contact</a> -->
           </div>
         </div>
       </div>
