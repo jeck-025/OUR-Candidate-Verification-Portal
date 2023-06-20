7465c6553a7f23186a3022bd35697a5621c58216
jeck-025 <jerrickanatalio@gmail.com>
2023-06-20 16:30:21 +0800

    report and cosmetics 7

--- admindash.php
+++ admindash.php
@@ -264,15 +264,15 @@ $adduser = new addAccount();
                 $mailer->updateMailerConfig(); 
                 ?>
 
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <!-- <div class="alert alert-success alert-dismissible fade show" role="alert">
                 <strong><i class="bi bi-check-circle"></i> Mailer Configuration Updated</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
+                </div> -->
 
-                <!-- <script> 
+                <script> 
                     alert('Mailer Configuration Updated');
                     location.replace('admindash.php'); 
-                </script> -->
+                </script>
 
         <?php }
             if(!empty($_POST['email'])){
@@ -288,10 +288,22 @@ $adduser = new addAccount();
         <div class="pagtitle pt-3 pb-3" data-aos="fade-in" data-aos-duration="1000">
             <h1>Dashboard</h1>
         </div>
+
+        <!-- COLLAPSE BUTTON---------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+        <p>
+            <button class="btn btn-sm btn-dark" type="button" data-toggle="collapse" data-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+                <i class="bi bi-chevron-double-down"></i> Show / Hide Verification Summary
+            </button>
+        </p>
+
+        <!-- COLLAPSE START---------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+        <div class="collapse <?php if(isset($_GET['date'])){echo "show";} ?>" id="collapseWidthExample">
+
+
         <section class="reports mb-3">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-4">
+                    <div class="col-md-6 mb-3">
                         <div class="card">
                             <div class="card-body report">
                                 <h3 class="report-title main-part mb-4">Office Verification Report</h3>
@@ -303,13 +315,17 @@ $adduser = new addAccount();
                                         $month = $strArray[1];
                                         $convDate = strtotime($_GET['date']);
                                         $display = date('F Y', $convDate);
-                                        echo"<h6><i class='bi bi-menu-button-wide-fill'></i>&nbsp Total Pending Verifications for the month of ".$display.": ";
-                                        echo"<br><span class='count mt-3'>".$view->pendingCount($year, $month)." pending verification/s<br>";
-                                        echo"<small>( MNL - <b>".$view->pendingCountMNL($year, $month)."</b> | MKT - <b>".$view->pendingCountMKT($year, $month)."</b> | MLS - <b>".$view->pendingCountMLS($year, $month)."</b> )</small></span></h6><br>";
+                                        echo"<h6><i class='bi bi-menu-button-wide-fill'></i>&nbsp Total Pending Verifications for the month of ".$display.": <br>";
+                                        echo"<span class='count mt-3'>".$view->pendingCount($year, $month)." pending verification/s<br>";
+                                        echo"<small> Manila <b>( ".$view->pendingCountMNL($year, $month)." )</b> &mdash;
+                                                     Makati <b>( ".$view->pendingCountMKT($year, $month)." )</b> &mdash;
+                                                     Malolos <b>( ".$view->pendingCountMLS($year, $month)." )</b></small></span></h6><br>";
                                     }else{
                                         echo"<h6><i class='bi bi-menu-button-wide-fill'></i>&nbsp Total Pending Verifications: ";
-                                        echo"<br><span class='count mt-3'>".$view->allPendingCount()." pending verification/s <br>";
-                                        echo"<small>( MNL - <b>".$view->allPendingCountMNL()."</b> | MKT - <b>".$view->allPendingCountMKT()."</b> | MLS - <b>".$view->allPendingCountMLS()."</b> )</small></span></h6><br>";
+                                        echo"<span class='count mt-3'>".$view->allPendingCount()." pending verification/s <br>";
+                                        echo"<small> Manila <b>( ".$view->allPendingCountMNL()." )</b> &mdash;
+                                                     Makati <b>( ".$view->allPendingCountMKT()." )</b> &mdash;
+                                                     Malolos <b>( ".$view->allPendingCountMLS()." )</b></small></span></h6><br>";
                                     }
 
                                     if(isset($_GET['date']) && ($_GET['date'] != "")){
@@ -318,13 +334,19 @@ $adduser = new addAccount();
                                         $month = $strArray[1];
                                         $convDate = strtotime($_GET['date']);
                                         $display = date('F Y', $convDate);
-                                        echo"<h6><i class='bi bi-calendar-fill'></i>&nbsp Total Verifications received for the month of ".$display.": ";
-                                        echo"<br><span class='count mt-3'>".$view->totalMonthByM($year, $month)." verification/s";
-                                        echo"<small> ( ".$view->totalMonthByMDone($year, $month)." processed )</small></span></h6><br>";
+                                        echo"<h6><i class='bi bi-calendar-fill'></i>&nbsp Total Verifications received for the month of ".$display.":<br> ";
+                                        echo"<span class='count mt-3'>".$view->totalMonthByM($year, $month)." verification/s";
+                                        echo"<small> ( ".$view->totalMonthByMDone($year, $month)." processed )</small><br>";
+                                        echo"<small> Manila <b>( ".$view->totalMonthByMMNL($year, $month)."</b> / ".$view->totalMonthByMDoneMNL($year, $month)." ) &mdash;
+                                                     Makati <b>( ".$view->totalMonthByMMKT($year, $month)."</b> / ".$view->totalMonthByMDoneMKT($year, $month)." ) &mdash;
+                                                     Malolos <b>( ".$view->totalMonthByMMLS($year, $month)."</b> / ".$view->totalMonthByMDoneMLS($year, $month)." )</small></span></h6><br>";
                                     }else{
                                         echo"<h6><i class='bi bi-calendar-fill'></i>&nbsp Total Verification/s received for this month: ";
-                                        echo"<br><span class='count mt-3'>".$view->totalMonth()." application/s";
-                                        echo"<small> ( ".$view->totalMonthDone()." processed )</small></span></h6><br>";
+                                        echo"<span class='count mt-3'>".$view->totalMonth()." verification/s<br>";
+                                        echo"<small> ( ".$view->totalMonthDone()." processed )</small><br>";
+                                        echo"<small> Manila <b>( ".$view->totalMonthMNL()."</b> / ".$view->totalMonthDoneMNL()." ) &mdash;
+                                                     Makati <b>( ".$view->totalMonthMKT()."</b> / ".$view->totalMonthDoneMKT()." ) &mdash;
+                                                     Malolos <b>( ".$view->totalMonthMLS()."</b> / ".$view->totalMonthDoneMLS()." )</small></span></h6><br>";
                                     }
 
                                     if(isset($_GET['date']) && ($_GET['date'] != "")){
@@ -333,17 +355,23 @@ $adduser = new addAccount();
                                         $month = $strArray[1];
                                         $convDate = strtotime($_GET['date']);
                                         $display = date('F Y', $convDate);
-                                        echo"<h6><i class='bi bi-patch-check-fill'></i>&nbsp Total Verifications Processed for the month of ".$display.": ";
-                                        echo"<br><span class='count mt-3'>".$view->totalMonthCompletedByM($year, $month)." verification/s</span></h6><br>";
+                                        echo"<h6><i class='bi bi-patch-check-fill'></i>&nbsp Total Verifications Processed for the month of ".$display.":<br> ";
+                                        echo"<span class='count mt-3'>".$view->totalMonthCompletedByM($year, $month)." verification/s<br>";
+                                        echo"<small> Manila <b>( ".$view->totalMonthCompletedbyMNL($year, $month)." )</b> &mdash;
+                                                     Makati <b>( ".$view->totalMonthCompletedbyMKT($year, $month)." )</b> &mdash;
+                                                     Malolos <b>( ".$view->totalMonthCompletedbyMLS($year, $month)." )</b></small></span></h6>";
                                     }else{
                                         echo"<h6><i class='bi bi-patch-check-fill'></i>&nbsp Total Verification/s Processed for this month: ";
-                                        echo"<br><span class='count mt-3'>".$view->totalMonthCompleted()." verification/s</span></h6>";
+                                        echo"<span class='count mt-3'>".$view->totalMonthCompleted()." verification/s<br>";
+                                        echo"<small> Manila <b>( ".$view->totalMonthCompletedMNL()." )</b> &mdash;
+                                                     Makati <b>( ".$view->totalMonthCompletedMKT()." )</b> &mdash;
+                                                     Malolos <b>( ".$view->totalMonthCompletedMLS()." )</b></small></span></h6>";
                                     }
                                     ?>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-6 mb-3">
                         <div class="card">
                             <div class="card-body report">
                                 <h3 class="report-title main-part mb-4">Breakdown of Verification Remarks</h3>
@@ -354,11 +382,17 @@ $adduser = new addAccount();
                                         $month = $strArray[1];
                                         $convDate = strtotime($_GET['date']);
                                         $display = date('F Y', $convDate);
-                                        echo "<h6><i class='bi bi-exclamation-octagon-fill icon'></i> On-Hold Verifications for the month of ".$display.": ";
-                                        echo "<br><span class='count'> ".$view->onHoldCount($year, $month)." verification/s</span></h6><br>";
+                                        echo "<h6><i class='bi bi-exclamation-octagon-fill icon'></i> On-Hold Verifications for the month of ".$display.": <br> ";
+                                        echo "<span class='count'> ".$view->onHoldCount($year, $month)." verification/s <br>";
+                                        echo"<small> Manila <b>( ".$view->onHoldCountMNL($year, $month)." )</b> &mdash;
+                                                      Makati <b>( ".$view->onHoldCountMKT($year, $month)." )</b> &mdash;
+                                                      Malolos <b>( ".$view->onHoldCountMLS($year, $month)." )</b></small></span></h6><br>";
                                     }else{
                                         echo "<h6><i class='bi bi-exclamation-octagon-fill icon'></i> Total On-Hold Verifications: ";
-                                        echo "<br><span class='count'> ".$view->allOnHoldCount()." verification/s</span></h6><br>";
+                                        echo "<span class='count'> ".$view->allOnHoldCount()." verification/s <br>";
+                                        echo"<small> Manila <b>( ".$view->allOnHoldCountMNL()." )</b> &mdash;
+                                                      Makati <b>( ".$view->allOnHoldCountMKT()." )</b> &mdash;
+                                                      Malolos <b>( ".$view->allOnHoldCountMLS()." )</b></small></span></h6><br>";
                                     }
 
                                     if(isset($_GET['date']) && ($_GET['date'] != "")) {
@@ -367,11 +401,17 @@ $adduser = new addAccount();
                                         $month = $strArray[1];
                                         $convDate = strtotime($_GET['date']);
                                         $display = date('F Y', $convDate);
-                                        echo "<h6><i class='bi bi-person-x-fill icon'></i> Denied Verifications for the month of ".$display.": ";
-                                        echo "<br><span class='count'> ".$view->DeniedCount($year, $month)." verification/s</span></h6><br>";
+                                        echo "<h6><i class='bi bi-person-x-fill icon'></i> Denied Verifications for the month of ".$display.": <br> ";
+                                        echo "<span class='count'> ".$view->deniedCount($year, $month)." verification/s<br>";
+                                        echo"<small> Manila <b>( ".$view->deniedCountMNL($year, $month)." )</b> &mdash;
+                                                     Makati <b>( ".$view->deniedCountMKT($year, $month)." )</b> &mdash;
+                                                     Malolos <b>( ".$view->deniedCountMLS($year, $month)." )</b></small></span></h6><br>";
                                     }else{
                                         echo "<h6><i class='bi bi-person-x-fill'></i> Total Denied Verifications: ";
-                                        echo "<br><span class='count'> ".$view->allDeniedCount()." verification/s</span></h6><br>";
+                                        echo "<span class='count'> ".$view->allDeniedCount()." verification/s<br>";
+                                        echo"<small> Manila <b>( ".$view->allDeniedCountMNL()." )</b> &mdash;
+                                                     Makati <b>( ".$view->allDeniedCountMKT()." )</b> &mdash;
+                                                     Malolos <b>( ".$view->allDeniedCountMLS()." )</b></small></span></h6><br>";
                                     }
 
                                     if(isset($_GET['date']) && ($_GET['date'] != "")) {
@@ -380,33 +420,48 @@ $adduser = new addAccount();
                                         $month = $strArray[1];
                                         $convDate = strtotime($_GET['date']);
                                         $display = date('F Y', $convDate);
-                                        echo "<h6><i class='bi bi-person-check-fill icon'></i> Verified for the month of ".$display.": ";
-                                        echo "<br><span class='count'> ".$view->approvedCount($year, $month)." verification/s</span></h6>";
+                                        echo "<h6><i class='bi bi-check2-all'></i> Verified for the month of ".$display.": <br>";
+                                        echo "<span class='count'> ".$view->approvedCount($year, $month)." verification/s<br>";
+                                        echo"<small> Manila <b>( ".$view->approvedCountMNL($year, $month)." )</b> &mdash;
+                                                     Makati <b>( ".$view->approvedCountMKT($year, $month)." )</b> &mdash;
+                                                     Malolos <b>( ".$view->approvedCountMLS($year, $month)." )</b></small></span></h6>";
                                     }else{
-                                        echo "<h6><i class='bi bi-person-check-fill icon'></i> Total Verified Requests: ";
-                                        echo "<br><span class='count'> ".$view->allApprovedCount()." verification/s</span></h6>";
+                                        echo "<h6><i class='bi bi-check2-all'></i> Total Verified Requests: ";
+                                        echo "<span class='count'> ".$view->allApprovedCount()." verification/s<br>";
+                                        echo"<small> Manila <b>( ".$view->allApprovedCountMNL()." )</b> &mdash;
+                                                     Makati <b>( ".$view->allApprovedCountMKT()." )</b> &mdash;
+                                                     Malolos <b>( ".$view->allApprovedCountMLS()." )</b></small></span></h6>";
                                     }
                                 ?>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="card">
-                            <div class="card-body report">
-                                <form action="" method="GET">
-                                    <h3 class="report-title main-part mb-4">View Other Monthly Report</h3>
-                                    <div class="text-center">
-                                    <input type="month" class="search datepicker mb-3" name="date" id="search" value="<?php  if(!empty($_GET['date'])){echo $_GET['date']; } ?>"></br>
-                                    <input type="submit" class="date_btn" name="month_btn" id="month_btn" value="Filter">
-                                    <br><a class="btn btn-info date_btn mt-2" href="admindash">Clear</a>
-                                    </div>
-                                </form>
+                </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body report">
+                                    <form action="" method="GET">
+                                        <div class="text-center">
+                                        <h5 class="report-title main-part">View Other Monthly Report</h5>
+                                        <input type="month" class="search datepicker mb-3" name="date" id="search" value="<?php  if(!empty($_GET['date'])){echo $_GET['date']; } ?>">
+                                        <input type="submit" class="date_btn btn" name="month_btn" id="month_btn" value="Filter">
+                                        <a class="btn btn-info date_btn" href="admindash">Clear</a>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+
+
             </div>
         </section>
+        </div>
+        <!-- COLLAPSE END---------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
+       
+        
         <section class="section dashboard" data-aos="fade-in" data-aos-duration="1000">
             <div class="row">
                 <div class="col-lg-12">
