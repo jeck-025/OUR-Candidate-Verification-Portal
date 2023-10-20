ce57c6a91a320e0ef91f51f06c8ab7131c17d753
jeck-025 <jerrickanatalio@gmail.com>
2023-10-20 09:20:24 +0800

    mailer message update

--- vendor/sendmail2.php
+++ vendor/sendmail2.php
@@ -18,17 +18,56 @@ $body ="<p>Dear Verification Officer ,</p>
 <p>Greetings of peace!</p>
 <p>You have successfully submitted a verification request for a Candidate Verification.</p>
 <p>Please take note of your verification reference number <b>$tn</b>.</p>
-<p>You may check the status of your request using our verification status checker at the CEU CAVE Portal Checker.</p>
+
+<p>You may check the status of your request using our verification status checker at the CEU CAVE Portal Checker: 
+  https://ceumnlregistrar.com/caveportal/scrstatus</p>
+
 <p>Verification will take 5-10 working days after submission</p>
-<p>If you have question or inquiry please send an email to: 
-<ul>
-  <li><b>rdmama@ceu.edu.ph</b> for CEU Manila</li>
-  <li><b>raparada@ceu.edu.ph</b> for CEU Makati</li>
-  <li><b>kdeleon@ceu.edu.ph</b> for CEU Malolos</li>
-</ul>
-</p>
-<p><b>This is an auto generated email please do not reply.</b></p>
-<p>Thank you and stay safe.</p>";
+
+<p>For additional assistance / inquiries, please email:</p>
+          <ul>
+            <li>For CEU Manila</li>
+              <ul>
+                <li><b>rdmama@ceu.edu.ph</b> - Ms. Rashida Mae Mama</li>
+                  <ul>
+                    <li>All Courses</li>
+                  </ul>
+              </ul> <br>
+          
+            <li>For CEU Malolos</li>
+              <ul>
+                <li><b>kdeleon@ceu.edu.ph</b> - Mr. Kenneth De Leon</li>
+                <ul>
+                  <li>All Courses</li>
+                </ul>
+            </ul> <br>
+
+            <li>For CEU Makati</li>
+              <ul>
+                <li><b>raparada@ceu.edu.ph</b> - Ms. Ruby Parada</li>
+                  <ul>
+                    <li>Doctor of Dental Medicine, BS Medical Technology, BS Pharmacy, Clinical Pharmacy, Doctor of Pharmacy, BS Psychology, Juris Doctor</li>      
+                  </ul> <br>
+
+                <li><b>rdmalate@ceu.edu.ph</b> - Mrs. Rizza Mae Malate-Arce</li>
+                  <ul>
+                    <li>BS Accountancy, BSBA Management / Marketing Management, BSBA International Marketing, BS Legal Management</li>      
+                  </ul> <br>
+
+                <li><b>mnantonio@ceu.edu.ph</b> - Mrs. Ma. Florencia Antonio</li>
+                  <ul>
+                    <li>BS Computer Science, BS Information Technology, BS Int'l Hosp. Mgt. - HRCO / CIRO</li>      
+                  </ul> <br>
+
+                <li><b>nlliwanag@ceu.edu.ph</b> - Mr. Nerwin Liwanag</li>
+                  <ul>
+                    <li>BS Int'l Tourism & Travel Mgt, Graduate School</li>      
+                  </ul> <br>
+              </ul>
+          </ul>
+
+<p><b>This is an auto-generated email message. Please do not reply here.</b></p>
+<p>Thank you.</p>";
 
 
 try {
@@ -66,3 +105,4 @@ try {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
     //header
+?>
